e962ed274ba2008a7c9276e5651b3d06c8ae79a5
Slavbor <shipmate@mail.ru>
2022-12-24 01:20:42 +0300

    Zadacha 19 ready

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+Write("Input number for polindrome check: ");
+
+long num = long.Parse(ReadLine()!);
+long result = 0;
+long num2 = num;
+while (num2 != 0)
+{
+    result *= 10;
+    result += num2 % 10;
+    num2 = num2 / 10;
+}
+Write(result == num ? "YES" : "NO");
