2694bdf0ffa56e5122e1a6da17f193147c965092
Slavbor <shipmate@mail.ru>
2022-12-24 19:40:42 +0300

    Zadacha 23 ready

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+Write("Enter number: ");
+long num = long.Parse(ReadLine()!);
+long i = 1;
+while (i <= num)
+{
+    Write(Math.Pow(i, 3));
+    i++;
+    if(i <= num)
+    {
+        Write(", ");
+    }
+    
+}
