a14dfa2b8ad60d45be5869fe11bc04bdf2a105e0
Slavbor <shipmate@mail.ru>
2022-12-25 01:51:13 +0300

    Zadacha19 convert to

---
+++ Zadacha19-2/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+Write("Input number for polindrome check: ");
+
+long n = long.Parse(ReadLine()!);
+long refer = n;
+string n2 = "";
+
+while (n != 0)
+{
+    string testnum = Convert.ToString(n % 10);
+    n2 = n2 + testnum;
+    n = n / 10;
+}
+Write(long.Parse(n2) == refer ? "YES" : "NO");
